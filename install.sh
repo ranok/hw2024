fef77d8bc1c9853c4473692e3d49d9d54adca186
Jacob Torrey <jacob@thinkst.com>
2024-11-26 09:26:33 -0700

    Added wifi config service
    Signed-off-by: Jacob Torrey <jacob@thinkst.com>

--- install.sh
+++ install.sh
@@ -5,5 +5,8 @@ sudo apt install python3-pip libfreetype-dev libjpeg-dev -y
 sudo apt-get install libopenblas*dev -y
 sudo mkdir -p /opt/cg
 sudo chown $(whoami):$(whoami) /opt/cg
+cd /opt/cg && git clone https://github.com/ranok/hw2024.git .
+cd /opt/cg && sudo ln -s /opt/cg/wificonfig.service /etc/systemd/system/wificonfig.service
+sudo systemctl enable --now wificonfig.service
 cd /opt/cg && python3 -m venv venv
-cd /opt/cg && source venv/bin/activate && pip install pillow numpy lgpio spidev gpiozero 
+cd /opt/cg && source venv/bin/activate && pip install pillow numpy lgpio spidev gpiozero flask
